d4cee1e563b855dfbba86d8ee3da80901f6ecf67
Utkarsh Pandey <166918722+utkarshpandeyyash@users.noreply.github.com>
2026-06-30 12:29:11 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+export default defineConfig({
+  base:'/Travelbuddywebsite/",
+})
 
 
 function figmaAssetResolver() {
